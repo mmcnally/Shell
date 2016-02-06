6d505bd07dbef3c1167912ae40cd90b9e76ee1fe
Matt McNally <mgmcnally@umass.edu>
2016-02-06 15:20:50 -0500

    added basic starter thing

---
+++ shell.py
@@ -0,0 +1,15 @@
+# wooo shellllllss
+import sys
+
+
+def print_prompt():
+    return '> '
+#    print('> ', end="", flush=True)
+
+def run_shell():
+    while True:
+        input(print_prompt())
+
+
+run_shell()
+    
